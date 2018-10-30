26b01589e29e5ee8f6b847a04c42f0f16baf60a3
forgetIt <2271404280@qq.com>
2018-10-30 19:01:12 +0800

    remove bootstrap sudo

--- bootstrap/prereqs.sh
+++ bootstrap/prereqs.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-sudo yum update -y
+yum update -y
 set -e
-sudo yum makecache fast
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-# sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo cp $(pwd)/docker-ce.repo /etc/yum.repos.d/docker-ce.repo
-sudo sed -i 's+download.docker.com+mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
+yum makecache fast
+yum install -y yum-utils device-mapper-persistent-data lvm2
+# yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+cp $(pwd)/docker-ce.repo /etc/yum.repos.d/docker-ce.repo
+sed -i 's+download.docker.com+mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
 yum list docker-ce --showduplicates | sort -r
-sudo yum install docker-ce -y
-sudo usermod -aG docker ${1:-$(whoami)}
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo yum install python2-pip -y
-sudo pip install docker-compose -i https://pypi.tuna.tsinghua.edu.cn/simple
+yum install docker-ce -y
+usermod -aG docker ${1:-$(whoami)}
+systemctl start docker
+systemctl enable docker
+yum install python2-pip -y
+pip install docker-compose -i https://pypi.tuna.tsinghua.edu.cn/simple
 echo ''
 echo 'Installation completed, versions installed are:'
 echo ''
